b1f3ce23cc8924f0a16955dded8a94ed62bc78af
Virginia Álvarez <virginiaalvarez.ar@gmail.com>
2022-12-21 11:20:37 +0100

    add express 2

--- src/index.js
+++ src/index.js
@@ -19,3 +19,10 @@ server.get('/movies', (req, res) => {
   };
   res.json(data);
 });
+
+const staticServerPathWeb = './src/public-react'; // Aquí hemos creado la carpeta public-react (estático)
+//esto funciona como el gitHub pages, contiene la última versión hasta que tu actualices con un npm run publish-react
+server.use(express.static(staticServerPathWeb));
+
+const staticImages = './src/public-movies-images';
+server.use(express.static(staticImages));
