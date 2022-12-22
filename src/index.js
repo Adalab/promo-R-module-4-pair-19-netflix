6918dc6e917c14df008601b8d564eb564b61606e
Natalia-GP <nataliag86@yahoo.es>
2022-12-22 11:43:48 +0100

    Express III 1-4

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+server.set('view engine', 'ejs'); // configuración del motor de plantillas
+
 // init express aplication
 const serverPort = 4000;
 server.listen(serverPort, () => {
@@ -19,6 +21,12 @@ server.get('/movies', (req, res) => {
   };
   res.json(data);
 });
+//servidor dinámico
+server.get('/movie/:movieId', (req, res) => {
+  const foundMovie = movies.find((movie) => movie.id === req.params.movieId);
+  console.log(foundMovie);
+  res.render('movie', foundMovie); //renderizamos la plantilla del html que hemos creado en la carpeta views dónde movies es el nombre del archivo y foundMovie es un objeto con los datos de la película
+});
 
 const staticServerPathWeb = './src/public-react'; // Aquí hemos creado la carpeta public-react (estático)
 //esto funciona como el gitHub pages, contiene la última versión hasta que tu actualices con un npm run publish-react
@@ -26,3 +34,6 @@ server.use(express.static(staticServerPathWeb));
 
 const staticImages = './src/public-movies-images';
 server.use(express.static(staticImages));
+
+const staticCss = './src/public-movies-css';
+server.use(express.static(staticCss));
