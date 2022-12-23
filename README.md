0d187a6a36bc30b8b19f7d90ead8bee6d9ff9ba4
Natalia-GP <nataliag86@yahoo.es>
2022-12-23 16:27:14 +0100

    Express JS II body params

--- README.md
+++ README.md
@@ -34,13 +34,13 @@ Por favor marca a continuación los ejericios que hayais terminado:
 
 - [x] 1. Pedir todas las películas
 - [] 2. Filtrar por género
-- [ ] 3. Ordenar por nombre
+- [] 3. Ordenar por nombre
 
   4.3 Express JS II:
 
 - [x] 1. Servidor de estáticos para React
 - [x] 2. Servidor de estáticos para las fotos
-- [-] 3. Peticiones POST con body params
+- [x] 3. Peticiones POST con body params
 - [ ] 4. Mantener logada a la usuaria
 
   4.4 Express JS III:
@@ -56,7 +56,7 @@ Por favor marca a continuación los ejericios que hayais terminado:
 - [x] 1. Crear la base de datos
 - [x] 2. Configura la base de datos en Node JS
 - [x] 3. Haz un SELECT para obtener todas películas
-- [ ] 4. Mejora tu SELECT
+- [-] 4. Mejora tu SELECT
 - [ ] 5. SELECT para el motor de plantillas
 - [ ] 6. Crear la tabla de usuarias
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const users = require('./data/users.json');
 // const movies = require('./data/movies.json');
 
 // importar la librería de la base de datos
 const Database = require('better-sqlite3');
 
-// importamos la base de datos 
+// importamos la base de datos
 const db = new Database('./src/db/database.db', {
   // con verbose le decimos que muestre en la consola todas las queries que se ejecuten
   verbose: console.log,
@@ -22,20 +23,44 @@ server.set('view engine', 'ejs'); // configuración del motor de plantillas
 // init express aplication
 const serverPort = 4000;
 server.listen(serverPort, () => {
-  console.log(`Server listening at http://localhost:${serverPort}`);
+  // console.log(`Server listening at http://localhost:${serverPort}`);
 });
+
 server.get('/movies', (req, res) => {
+  console.log(req);
+  const filterGender = req.query.gender;
   // preparamos la query
-const query = db.prepare('SELECT * FROM movies');
-// ejecutamos la query
-const movies = query.all();
-//pintabamos las peliculas del fichero movies.json
+  const query = db.prepare('SELECT * FROM movies');
+  // ejecutamos la query
+  const movies = query.all();
+
+  //pintabamos las peliculas del fichero movies.json
   const data = {
     success: true,
     movies: movies,
   };
   res.json(data);
 });
+
+server.post('/login', (req, res) => {
+ 
+  const userFound = users.find(
+    (user) =>
+      user.email === req.body.email && user.password === req.body.password
+  );
+  if (userFound) {
+    res.json({
+      success: true,
+      userId: 'id_de_la_usuaria_encontrada',
+    });
+  } else {
+    res.json({
+      success: false,
+      errorMessage: 'Usuaria/o no encontrada/o',
+    });
+  }
+});
+
 //servidor dinámico
 server.get('/movie/:movieId', (req, res) => {
   const foundMovie = movies.find((movie) => movie.id === req.params.movieId);
